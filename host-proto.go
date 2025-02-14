bbc02bab04853f669b89d9300303430504545074
John McAleely <john@mcaleely.com>
2025-02-14 19:15:06 +0000

    read a uf2 frame

--- host-proto.go
+++ host-proto.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Uf2Frame struct {
+	MagicStart0 uint32
+	MagicStart1 uint32
+	Flags       uint32
+	TargetAddr  uint32
+	PayloadSize uint32
+	BlockNo     uint32
+	NumBlocks   uint32
+	Reserved    uint32
+	Data        [476]byte
+	MagicEnd    uint32
+}
 
 func main() {
-	fmt.Println("Hello, proto-host")
+	f, err := os.Open("test.uf2")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+
+	uf := Uf2Frame{}
+	err = binary.Read(f, binary.LittleEndian, &uf)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Print(uf.NumBlocks)
+	fmt.Print(uf.BlockNo)
+	fmt.Print(uf.MagicStart0)
+	fmt.Print(uf.Flags)
 }

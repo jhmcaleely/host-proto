48d5464262c856615699d3a25c596f204f61e686
John McAleely <john@mcaleely.com>
2025-02-19 17:26:07 +0000

    rename cgo proxies

--- bdfs_lfs_hal_cgo.go
+++ bdfs_lfs_hal_cgo.go
@@ -17,17 +17,17 @@ package main
 // A start location counted back from the end of the device.
 #define FLASHFS_BASE_ADDR (PICO_FLASH_BASE_ADDR + PICO_FLASH_SIZE_BYTES - FLASHFS_SIZE_BYTES)
 
-int go_bdfs_read_cgo(const struct lfs_config* c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size);
-int go_bdfs_prog_page_cgo(const struct lfs_config* c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size);
-int go_bdfs_erase_block_cgo(const struct lfs_config* c, lfs_block_t block);
+int bdfs_read_cgo(const struct lfs_config* c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size);
+int bdfs_prog_page_cgo(const struct lfs_config* c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size);
+int bdfs_erase_block_cgo(const struct lfs_config* c, lfs_block_t block);
 int sync_block_nop(const struct lfs_config *c);
 
 // configuration of the filesystem is provided by this struct
 struct lfs_config cfg = {
     // block device operations
-    .read  = go_bdfs_read_cgo,
-    .prog  = go_bdfs_prog_page_cgo,
-    .erase = go_bdfs_erase_block_cgo,
+    .read  = bdfs_read_cgo,
+    .prog  = bdfs_prog_page_cgo,
+    .erase = bdfs_erase_block_cgo,
     .sync  = sync_block_nop,
 
     // block device configuration
@@ -71,19 +71,19 @@ int go_bdfs_read(struct block_device* bd, uint32_t fs_base_address, lfs_block_t 
 int go_bdfs_prog_page(struct block_device* bd, uint32_t fs_base_address, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size);
 int go_bdfs_erase_block(struct block_device* bd, uint32_t fs_base_address, lfs_block_t block);
 
-int go_bdfs_read_cgo(const struct lfs_config* c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size) {
+int bdfs_read_cgo(const struct lfs_config* c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size) {
 
 	struct flash_fs* fs = c->context;
     return go_bdfs_read(fs->device, fs->fs_flash_base_address, block, off, buffer, size);
 }
 
-int go_bdfs_prog_page_cgo(const struct lfs_config* c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size) {
+int bdfs_prog_page_cgo(const struct lfs_config* c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size) {
 
 	struct flash_fs* fs = c->context;
 	return go_bdfs_prog_page(fs->device, fs->fs_flash_base_address, block, off, buffer, size);
 }
 
-int go_bdfs_erase_block_cgo(const struct lfs_config* c, lfs_block_t block) {
+int bdfs_erase_block_cgo(const struct lfs_config* c, lfs_block_t block) {
 
 	struct flash_fs* fs = c->context;
     return go_bdfs_erase_block(fs->device, fs->fs_flash_base_address, block);

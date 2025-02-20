c7e76ca888c528fa7651b885642276683827d32a
John McAleely <john@mcaleely.com>
2025-02-20 21:56:09 +0000

    a hacky file addition, plus ls.

--- host-proto.go
+++ host-proto.go
@@ -97,6 +97,51 @@ func mount_and_update_boot(fs *BdFS) {
 	update_boot_count(lfsp)
 }
 
+func add_file(fs *C.lfs_t, fileToAdd string) {
+
+	r, err := os.Open(fileToAdd)
+	if err != nil {
+		fmt.Println(("nothing to open"))
+		os.Exit(1)
+	}
+	defer r.Close()
+	data, err := io.ReadAll(r)
+	if err != nil {
+		fmt.Println("nothing read")
+		os.Exit(1)
+	}
+
+	var lfsfile C.lfs_file_t
+	var pin runtime.Pinner
+	defer pin.Unpin()
+
+	filep := &lfsfile
+	pin.Pin(filep)
+
+	C.lfs_file_open(fs, filep, C.CString(fileToAdd), C.open_flags)
+	defer C.lfs_file_close(fs, filep)
+
+	C.lfs_file_write(fs, filep, unsafe.Pointer(&data[0]), C.lfs_size_t(len(data)))
+}
+
+func mount_and_add_file(fs *BdFS, fileToAdd string) {
+	var lfs C.lfs_t
+	var pin runtime.Pinner
+	defer pin.Unpin()
+
+	lfsp := &lfs
+	pin.Pin(lfsp)
+
+	lfsres := C.lfs_mount(lfsp, fs.LfsP)
+	if lfsres != 0 {
+		C.lfs_format(lfsp, fs.LfsP)
+		C.lfs_mount(lfsp, fs.LfsP)
+	}
+	defer C.lfs_unmount(lfsp)
+
+	add_file(lfsp, fileToAdd)
+}
+
 func bootCountDemo(fsFilename string) {
 	f, err := os.OpenFile(fsFilename, os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
@@ -126,10 +171,112 @@ func bootCountDemo(fsFilename string) {
 	bdWriteToUF2(device, f)
 }
 
+func addFile(fsFilename, fileToAdd string) {
+	f, err := os.OpenFile(fsFilename, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+
+	device := C.bdCreate(C.uint32_t(PICO_FLASH_BASE_ADDR))
+	defer C.bdDestroy(device)
+
+	fs := newBdFS(device)
+	var pin runtime.Pinner
+	pin.Pin(fs.LfsP)
+	defer pin.Unpin()
+	pin.Pin(fs.FsP)
+
+	defer fs.Close()
+
+	fs.init(FLASHFS_BASE_ADDR, FLASHFS_BLOCK_COUNT)
+
+	bdReadFromUF2(device, f)
+
+	mount_and_add_file(fs, fileToAdd)
+
+	f.Seek(0, io.SeekStart)
+
+	bdWriteToUF2(device, f)
+}
+
+func list_files(fs *C.lfs_t, dirEntry string) {
+
+	var lfsdir C.lfs_dir_t
+	var pin runtime.Pinner
+	defer pin.Unpin()
+	lfsdp := &lfsdir
+	pin.Pin(lfsdp)
+
+	err := C.lfs_dir_open(fs, lfsdp, C.CString(dirEntry))
+	if err != 0 {
+		fmt.Println("dir not opened")
+		os.Exit(10)
+	}
+	defer C.lfs_dir_close(fs, lfsdp)
+
+	var info C.struct_lfs_info
+	for i := C.lfs_dir_read(fs, lfsdp, &info); i > 0; i = C.lfs_dir_read(fs, lfsdp, &info) {
+		fmt.Println(C.GoString(&info.name[0]))
+	}
+
+}
+
+func mount_and_ls(fs *BdFS, dirEntry string) {
+	var lfs C.lfs_t
+	var pin runtime.Pinner
+	defer pin.Unpin()
+
+	lfsp := &lfs
+	pin.Pin(lfsp)
+
+	lfsres := C.lfs_mount(lfsp, fs.LfsP)
+	if lfsres != 0 {
+		C.lfs_format(lfsp, fs.LfsP)
+		C.lfs_mount(lfsp, fs.LfsP)
+	}
+	defer C.lfs_unmount(lfsp)
+
+	list_files(lfsp, dirEntry)
+}
+
+func lsDir(fsFilename, dirEntry string) {
+	f, err := os.OpenFile(fsFilename, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+
+	device := C.bdCreate(C.uint32_t(PICO_FLASH_BASE_ADDR))
+	defer C.bdDestroy(device)
+
+	fs := newBdFS(device)
+	var pin runtime.Pinner
+	pin.Pin(fs.LfsP)
+	defer pin.Unpin()
+	pin.Pin(fs.FsP)
+
+	defer fs.Close()
+
+	fs.init(FLASHFS_BASE_ADDR, FLASHFS_BLOCK_COUNT)
+
+	bdReadFromUF2(device, f)
+
+	mount_and_ls(fs, dirEntry)
+}
+
 func main() {
 
 	bootCountDemoCmd := flag.NewFlagSet("bootcount", flag.ExitOnError)
 	bootCountFS := bootCountDemoCmd.String("fs", "test.uf2", "mount and increment boot_count on fs")
+
+	addFileCmd := flag.NewFlagSet("addfile", flag.ExitOnError)
+	addFileFS := addFileCmd.String("fs", "test.uf2", "add file to this filesystem")
+	addFileName := addFileCmd.String("add", "", "filename to add")
+
+	lsDirCmd := flag.NewFlagSet("ls", flag.ExitOnError)
+	lsDirFS := lsDirCmd.String("fs", "test.uf2", "filesystem to mount")
+	lsDirEntry := lsDirCmd.String("dir", "/", "directory to ls")
 
 	if len(os.Args) < 2 {
 		fmt.Println("expected command")
@@ -140,6 +287,16 @@ func main() {
 	case "bootcount":
 		bootCountDemoCmd.Parse(os.Args[2:])
 		bootCountDemo(*bootCountFS)
+	case "addfile":
+		addFileCmd.Parse(os.Args[2:])
+		if *addFileName == "" {
+			fmt.Println("expect filename to add")
+			os.Exit(1)
+		}
+		addFile(*addFileFS, *addFileName)
+	case "ls":
+		lsDirCmd.Parse((os.Args[2:]))
+		lsDir(*lsDirFS, *lsDirEntry)
 	default:
 		fmt.Println("unknown command")
 		os.Exit(1)
